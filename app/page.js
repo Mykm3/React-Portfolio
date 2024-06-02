413f974152b27734867f8971738049e551e64cf8
Mykm3 <michaelmanu2019@gmail.com>
2024-06-02 16:55:09 +0000

    Initial commit from Create Next App

---
+++ app/page.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+        <Head>
+          <title>Portfolio</title>
+          <meta name="description" content="generated"></meta>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main>
+          <section className="bg-blue-700 min-h-screen">
+              <h1>Hello</h1>
+
+          </section>
+        </main>
+    </div>
+  );
+}
